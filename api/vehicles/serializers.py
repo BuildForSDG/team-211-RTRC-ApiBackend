96154003144ad8e7015d92a4a5c0077343becfeb
wahabawudu <wahabawudu2020@gmail.com>
2020-06-07 09:09:44 +0000

    added funds field to vehicles data

--- api/vehicles/serializers.py
+++ api/vehicles/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import VehicleCategory, Vehicle
 from api.users.serializers import UserSerializer
+from api.wallet.models import Wallet
 
 
 class VehicleCategorySerializer(ModelSerializer):
@@ -14,15 +15,26 @@ class VehicleCategorySerializer(ModelSerializer):
 class VehicleSerializer(ModelSerializer):
     category = VehicleCategorySerializer(read_only=True)
     user = UserSerializer(read_only=True)
+    sufficient_funds = SerializerMethodField()
 
     class Meta:
         model = Vehicle
         fields = [
             'id', 'user', 'license_number', 'registration_number', 'model',
-            'category', 'chassis_number', 'qr_code',
+            'category', 'chassis_number', 'qr_code', 'sufficient_funds',
             'created_at', 'updated_at',
         ]
         read_only_fields = ('id',)
     
+    def get_sufficient_funds(self, obj):
+        toll_fee = obj.category.toll_fee
+        wallet = Wallet.objects.get(user=obj.user)
+        wallet_funds = wallet.balance
+
+        if wallet_funds > toll_fee:
+            return True
+        else:
+            return False
+
 
 
