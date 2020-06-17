eef29bc652e4c510a65f3bfbdcc88d29ffd05aa8
wahabawudu <wahabawudu2020@gmail.com>
2020-06-17 00:03:37 +0000

    added new fields to wallet serializer

--- api/wallet/serializers.py
+++ api/wallet/serializers.py
@@ -9,14 +9,30 @@ from api.tolls.serializers import TollSerializer
 
 class WalletSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    total_deposits = serializers.SerializerMethodField()
+    total_transactions = serializers.SerializerMethodField()
 
     class Meta:
         model = Wallet
-        fields = ['id',
-                  'user',
-                  'balance',
-                  'created_at', 'updated_at']
-        read_only_fields = ('id', 'user', 'balance', 'created_at', 'updated_at',)    
+        fields = [
+            'id', 'user', 'balance', 'total_deposits', 'total_transactions',
+            'created_at', 'updated_at'
+            ]
+        read_only_fields = ('id', 'user', 'balance', 'created_at', 'updated_at',)
+
+    def get_total_deposits(self, obj):
+        deposits = Deposit.objects.filter(wallet=obj)
+        total = 0.00
+        for dep in deposits:
+            total += float(dep.amount)
+        return total
+    
+    def get_total_transactions(self, obj):
+        trans = Transaction.objects.filter(wallet=obj)
+        total = 0.00
+        for tran in trans:
+            total += float(tran.amount)
+        return total
 
 
 class DepositSerializer(serializers.ModelSerializer):
